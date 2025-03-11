d16f9f8e9bc2e663a3afbd82e92141d1a9f02490
yasmink88 <yasmin.khalaf.a@gmail.com>
2025-03-10 21:52:26 -0300

    Adding php arrays and data

--- components/hero.php
+++ components/hero.php
@@ -1,17 +1,38 @@
+<?php
+    $name        = 'Yasmin Khalaf';
+    $job         = 'Desenvolvedora PHP';
+    $description = 'Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.';
+    
+    $stacks = [
+        'Github',
+        'PHP',
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ];
+
+    $colors = [
+        'fuchsia',
+        'lime',
+        'sky',
+        'red',
+        'amber',
+    ]
+?>
+
+
 <div class="flex flex-col items-center text-gray-100 text-center gap-20 py-20">
     <img src="../assets/dev.png" alt="Dev image" width="175" height="175">
     <div class="content gap-2 flex flex-col items-center justify-center">
-        <p class="text-[20px]">Hello World! Meu nome é <span class="text-red-400"> Martina Santos </span> e sou</p>
-        <h1 class="text-[56px]">Desenvolvedora PHP</h1>
-        <p class="text-gray-400 text-[14px] w-1/2">Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.</p>
+        <p class="text-[20px]">Hello World! Meu nome é <span class="text-red-400"> <?= $name ?> </span> e sou</p>
+        <h1 class="text-[56px]"><?= $job ?></h1>
+        <p class="text-gray-400 text-[14px] w-1/2"><?= $description ?></p>
     </div>
     <div class="stack">
         <ul class="flex flex-row gap-2 text-gray-900">
-            <li class="bg-lime-400 py-1 px-4 flex items-center justify-center rounded-full">Github</li>
-            <li class="bg-fuchsia-400 py-1 px-4 flex items-center justify-center rounded-full">PHP</li>
-            <li class="bg-sky-400 py-1 px-4 flex items-center justify-center rounded-full">HTML</li>
-            <li class="bg-red-400 py-1 px-4 flex items-center justify-center rounded-full">CSS</li>
-            <li class="bg-amber-400 py-1 px-4 flex items-center justify-center rounded-full">JavaScript</li>
+            <?php foreach ( $colors as $position => $color ) : ?>
+                <li class="bg-<?= $color ?>-400 py-1 px-4 flex items-center justify-center rounded-full"><?= $stacks[$position] ?></li>
+            <?php endforeach; ?>
         </ul>
     </div>
 </div>
--- components/projects.php
+++ components/projects.php
@@ -1,42 +1,107 @@
+<?php
+    $colors = [
+        'fuchsia',
+        'lime',
+        'sky',
+        'red',
+        'amber',
+    ];
+
+    $title    = 'Meu Trabalho';
+    $subtitle = 'Veja os projetos em destaque';
+
+    $projects = [
+        [
+            'name' => 'Travelgram',
+            'description' => 'Rede social onde as pessoas mostram os registros de suas viagens pelo mundo',
+            'image' => '../assets/image.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ],
+        [
+            'name' => 'Página de receita',
+            'description' => 'Página com o passo a passo de uma receita para cupcakes',
+            'image' => '../assets/image-1.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ],
+        [
+            'name' => 'Tech News',
+            'description' => 'Homepage de um portal de notícias sobre a área de tecnologia',
+            'image' => '../assets/image-2.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ],
+        [
+            'name' => 'Refund',
+            'description' => 'Um sistema para pedido e acompanhamento de reembolso',
+            'image' => '../assets/Thumbnail_Project-04.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ],
+        [
+            'name' => 'Página de turismo',
+            'description' => 'Página com as principais informações para quem quer viajar para Busan',
+            'image' => '../assets/Thumbnail_Project-05.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ],
+        [
+            'name' => 'Zingen',
+            'description' => 'RLanding Page completa e responsiva de um aplicativo de Karaokê',
+            'image' => '../assets/Thumbnail_Project-06.png',
+            'stacks' => [
+                'PHP',
+                'HTML',
+                'CSS',
+                'JavaScript'
+            ]
+        ]
+    ];
+?>
+
+
 <div class="flex flex-col items-center text-gray-100 text-center gap-10 py-20">
     <div class=" title">
-        <h2 class="text-red-400 text-[20px]">Meu Trabalho</h2>
-        <span class="text-[24px]">Veja os projetos em destaque</span>
+        <h2 class="text-red-400 text-[20px]"><?= $title ?></h2>
+        <span class="text-[24px]"><?= $subtitle ?></span>
     </div>
     <div class="projects grid grid-cols-2 gap-10 w-full">
-        <div class="project bg-gray-800 rounded-2xl p-4 flex gap-6 flex-1">
-            <img class="w-1/3 h-auto object-cover rounded-2xl" src="../assets/image-1.png" alt="Project image">
-            <div class="text-left flex flex-col gap-4 flex-2">
-                <h3 class="text-[16px]">Travelgram</h3>
-                <p class="text-[14px]">Rede social onde as pessoas mostram os registros de suas viagens pelo mundo</p>
-                <div class="stack">
-                    <ul class="flex flex-row gap-2 text-gray-900 flex-wrap text-[12px]">
-                        <li class="bg-fuchsia-400 py-1 px-4 flex items-center justify-center rounded-full">PHP</li>
-                        <li class="bg-lime-400 py-1 px-4 flex items-center justify-center rounded-full">HTML</li>
-                        <li class="bg-red-400 py-1 px-4 flex items-center justify-center rounded-full">CSS</li>
-                        <li class="bg-amber-400 py-1 px-4 flex items-center justify-center rounded-full">JavaScript</li>
-                    </ul>
+        <?php foreach ($projects as $project ) : ?>
+            <div class="project bg-gray-800 rounded-2xl p-4 flex gap-6 flex-1">
+                <img class="w-1/3 h-auto object-cover rounded-2xl" src="<?= $project['image'] ?>" alt="Project image">
+                <div class="text-left flex flex-col gap-4 flex-2">
+                    <h3 class="text-[16px]"><?= $project['name'] ?></h3>
+                    <p class="text-[14px]"><?= $project['description'] ?></p>
+                    <div class="stack">
+                        <ul class="flex flex-row gap-2 text-gray-900 flex-wrap text-[12px]">
+                            <?php foreach ( $project['stacks'] as $position => $stack ) : ?>
+                            <li class="bg-<?= $colors[$position] ?>-400 py-1 px-4 flex items-center justify-center rounded-full"><?= $stack ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="project bg-gray-800 rounded-2xl p-4 flex gap-8 flex-1">
-            <img class="w-1/3 h-auto object-cover rounded-2xl" src="../assets/image-1.png" alt="Project image">
-            <div class="text-left flex flex-col gap-4 flex-2">
-                <h3 class="text-[16px]">Travelgram</h3>
-                <p class="text-[14px]">Rede social onde as pessoas mostram os registros de suas viagens pelo mundo</p>
-                <div class="stack">
-                    <ul class="flex flex-row gap-2 text-gray-900 flex-wrap text-[12px]">
-                        <li class="bg-fuchsia-400 py-1 px-4 flex items-center justify-center rounded-full">PHP</li>
-                        <li class="bg-lime-400 py-1 px-4 flex items-center justify-center rounded-full">HTML</li>
-                        <li class="bg-red-400 py-1 px-4 flex items-center justify-center rounded-full">CSS</li>
-                        <li class="bg-amber-400 py-1 px-4 flex items-center justify-center rounded-full">JavaScript</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        
-        
+        <?php endforeach; ?>
     </div>
 </div>
